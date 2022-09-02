c0e80910a853e69cc017123bb5cbe58dc200d9b1
staten09 <staten09@gmail.com>
2022-09-02 10:07:04 -0500

    added nearby search

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,5 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
-import { map } from 'rxjs';
 declare const google: any;
 @Component({
   selector: 'app-map',
@@ -11,22 +10,12 @@ export class MapComponent implements OnInit{
   title = 'google-maps';
 
   ngOnInit(): void {
-    const shelterIcon={
-      url:'https://cdn-icons-png.flaticon.com/512/9/9773.png',
-    scaledSize: {height: 30, width: 30},
-  }
-
     let loader = new Loader({
       apiKey: 'AIzaSyAVUb8DbYfWr3ylaxt5hMIuhmZeBeSfiJM',
       libraries: ['geometry', 'places']
     })
 
     loader.load().then(() => {
-<<<<<<< HEAD
-    let map = new google.maps.Map(document.getElementById("map"),{
-        center: {lat: 38.6270, lng: -90.1994},
-        zoom: 10,
-=======
 
     let map: google.maps.Map
     let infoWindow: google.maps.InfoWindow
@@ -44,22 +33,7 @@ export class MapComponent implements OnInit{
         position: place.geometry.location,
         title: place.name,
         map: map
->>>>>>> Basic-Nearby-Search
       })
-
-    addmarker({lat: 38.80505, lng: -90.32967})
-    addmarker({lat: 38.81834, lng: -90.47679})
-    addmarker({lat: 38.80443, lng: -90.57636})
-    
-    
-
-      function addmarker(coords={}){
-        let marker = new google.maps.Marker({
-        position:coords,
-        map: map,
-        icon: shelterIcon
-      });  
-      }
     })
 
     function initMap(): void {
