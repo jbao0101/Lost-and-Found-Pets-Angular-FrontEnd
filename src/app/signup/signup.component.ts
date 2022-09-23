b26eebc523fb4c27e6d9f2ff88a7878ded7a0345
Joe Bao <toj7278@yahoo.com>
2022-09-22 20:23:58 -0500

    Played around with signup.component.ts

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -14,6 +14,28 @@ export class SignupComponent implements OnInit {
   }
 
   onPetCreate(pet: {name: string, status: string, ownerFirstName: string, ownerLastName: string, contactNumber: string, contactEmail: string, microchipID:string,description:string}){
+    
+    fetch("http://localhost:8080", { method: 'POST', credentials: "include"})
+    .then(res => res.json())
+    .then(data => console.log(data))
+    
+    const express = require("express")
+    const app = express()
+    const cors = require("cors")
+
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+      })
+    )
+
+      app.get("/data", (req: any, res: { json: (arg0: { name: string; }) => void; }) => {
+        res.json({ name: "Kyle"})
+      })
+
+      app.listen(8080)
+
     console.log(pet)
     this.http.post('http://localhost:8080/pet/add/pet.json',pet)
     .subscribe((res) => {
