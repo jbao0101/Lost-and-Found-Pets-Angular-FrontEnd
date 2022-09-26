997648e8b9e58887903360e9c565c0a91ee66382
staten09 <staten09@gmail.com>
2022-09-25 20:26:54 -0500

    working on CORS

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -15,31 +15,31 @@ export class SignupComponent implements OnInit {
 
   onPetCreate(pet: {name: string, status: string, ownerFirstName: string, ownerLastName: string, contactNumber: string, contactEmail: string, microchipID:string,description:string}){
     
-    fetch("http://localhost:8080", { method: 'POST', credentials: "include"})
+    fetch("http://localhost:8080/pet/add", { method: 'POST', credentials: "include"})
     .then(res => res.json())
     .then(data => console.log(data))
     
-    const express = require("express")
-    const app = express()
-    const cors = require("cors")
+    // const express = require("express")
+    // const app = express()
+    // const cors = require("cors")
 
-    app.use(
-      cors({
-        origin: "*",
-        credentials: true,
-      })
-    )
+    // app.use(
+    //   cors({
+    //     origin: "*",
+    //     credentials: true,
+    //   })
+    // )
 
-      app.get("/data", (req: any, res: { json: (arg0: { name: string; }) => void; }) => {
-        res.json({ name: "Kyle"})
-      })
+    //   app.get("/data", (req: any, res: { json: (arg0: { name: string; }) => void; }) => {
+    //     res.json({ name: "Kyle"})
+    //   })
 
-      app.listen(8080)
+    //   app.listen(8080)
 
-    console.log(pet)
-    this.http.post('http://localhost:8080/pet/add/pet.json',pet)
-    .subscribe((res) => {
-      console.log(res);
-    });
+    // console.log(pet)
+    // this.http.post('http://localhost:8080/pet/add/pet.json',pet)
+    // .subscribe((res) => {
+    //   console.log(res);
+    // });
   }
 }
